62981c994f0f96b02195138f8ca9c8e292c50838
Hannah Kristine <hannahsoliao18@gmail.com>
2022-10-09 01:59:14 +0400

    Create Chapter_2.py

---
+++ Chapter 2- Variables & Comments/Exercises/Chapter_2.py
@@ -0,0 +1,13 @@
+# This is the solution to Excercise 1
+
+sentence_1 = "Hello this is the start of human mass extinction."
+print(sentence_1)
+
+sentence_1 = "Bomb will detonate in 3 seconds."
+print(sentence_1)
+
+# This is the solution to Excercise 2
+quote = "My dog stepped on a bee"
+author = "Amber Heard"
+
+print(author, "once said, ", quote)
