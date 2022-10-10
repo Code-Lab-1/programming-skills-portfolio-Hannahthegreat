0a9b98a06cd75abb4eacf98f7640fd0872719986
Hannah Kristine <hannahsoliao18@gmail.com>
2022-10-10 12:44:35 +0400

    Update Chapter_2.py

--- Chapter 2- Variables & Comments/Exercises/Chapter_2.py
+++ Chapter 2- Variables & Comments/Exercises/Chapter_2.py
@@ -11,3 +11,27 @@ quote = "My dog stepped on a bee"
 author = "Amber Heard"
 
 print(author, "once said, ", quote)
+
+# This is the solution to Excercise 3
+
+list = ['\tPatrice\n', '\tSandy\n', '\tPrincess\n']
+print(list[0].lstrip())
+print(list[1].rstrip())
+print(list[2].strip())
+
+
+# This is the solution to Excercise 4
+fav_num = 14
+
+print("My lucky number is ", fav_num)
+
+# This is the solution to Excercise 5
+
+usb = 6
+money = 50
+
+result = int(money/usb)
+change = money - (result * usb)
+print("The equation: {0} / {1}\n".format(usb,money))
+print("She will get {0} amount of sticks".format(result))
+print("Her change is: {0}".format(change))
