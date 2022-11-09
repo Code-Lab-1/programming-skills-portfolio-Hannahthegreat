5f716a47278a81484e8f40de835e4fdf2fa668ce
Hannah Kristine <hannahsoliao18@gmail.com>
2022-11-09 13:06:36 +0400

    Update Chapter_6.py

--- Chapter 6- Loops/Exercises/Chapter_6.py
+++ Chapter 6- Loops/Exercises/Chapter_6.py
@@ -1,7 +1,7 @@
 # This is the solution to Excercise 1
 
 print("\tWelcome to freddie's pizzaria")
-print("\nadd as many toppings as you want. \nWhen it's enough type\n\t 'QUIT'")
+print("\nadd as many toppings as you want. \nWhen it's enough type\n\t 'QUIT'") 
 
 while 1:
     topping = input("Insert Topping: ")
@@ -27,7 +27,38 @@ while 1:
         print("You have entered an invalid input")
         print("Please try again,,,")
 
-# This is the solution to Excercise 2
+# This is the solution to Excercise 3
 
 while 1:
     print("This is an infinite loop")
+
+# This is the solution to Excercise 4
+
+sandwhich_orders = ["egg sandwhich","hotdog sandwhich","steak sandwhich"] #sandwhich orders list 
+finished_sandwhiches = [] # empty finished sandwhiches list
+print(len(finished_sandwhiches)) #length of empty list
+
+for x in sandwhich_orders : #for loop: print sandwhiches and append each variable x in finished sandwhiches
+    finished_sandwhiches.append(x)
+    print("I have made your", x)
+    print(finished_sandwhiches)
+
+
+# This is the solution to Excercise 5
+sandwhich_orders = ["egg sandwhich","pastrami","hotdog sandwhich","pastrami","steak sandwhich","pastrami"] #sandwhich orders list 
+finished_sandwhiches = [] # empty finished sandwhiches list
+
+print("Deli has unfortunately ran out of pastrami")
+print("\n")
+
+while 'pastrami' in sandwhich_orders:
+    sandwhich_orders.remove('pastrami')
+    print("Removing pastrami>>>",sandwhich_orders)
+
+print("\n")
+
+for x in sandwhich_orders: 
+    finished_sandwhiches.append(x)
+    print("I have made your", x)
+
+print("\nFinished Sandwhiches:",finished_sandwhiches)
